96dc9e19e01815ed850a6d3e477db52483902995
rrxe <rayanrayansra@gmail.com>
2026-09-10 17:05:22 +0300

    suppress TS error for adsgram-task tags

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -307,11 +307,13 @@ export function Tasks({
             <span>نفّذ الإعلان واربح +1 نقطة</span>
           </div>
           <div className="adsgram-native-task-host">
+            {/* @ts-expect-error AdsGram web component */}
             <adsgram-task ref={nativeTaskElRef} data-block-id={ADSGRAM_NATIVE_TASK_BLOCK_ID} data-debug="false">
               <span slot="reward">+1</span>
               <span slot="button">{nativeTaskBusy ? 'جاري التحقق...' : 'فتح الإعلان'}</span>
               <span slot="claim">استلام +1</span>
               <span slot="done">تمت المهمة ✓</span>
+            {/* @ts-expect-error AdsGram web component */}
             </adsgram-task>
           </div>
         </article>
